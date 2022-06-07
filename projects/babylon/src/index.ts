4f2969f73ee6debcb944d39952d6f9e86464cade
Kefniark <kevin@destrem.net>
2022-06-07 22:59:48 +0900

    Add onload event listener

--- projects/babylon/src/index.ts
+++ projects/babylon/src/index.ts
@@ -3,20 +3,22 @@ import App from "./app.vue"
 import IntroScene from "@src/scenes/intro.vue"
 import MenuScene from "@src/scenes/menu.vue"
 
-// create Application
-mountApp("#app", App, {
-  routes: [
-    { path: "/", component: IntroScene },
-    { path: "/menu", component: MenuScene },
-    { path: "/main", component: () => import("./scenes/main.vue") },
-  ],
-  locales: ["en", "ja"],
-  messages: {
-    en: {
-      message: "Hello",
+window.addEventListener('load', () => {
+  // create Application
+  mountApp("#app", App, {
+    routes: [
+      { path: "/", component: IntroScene },
+      { path: "/menu", component: MenuScene },
+      { path: "/main", component: () => import("./scenes/main.vue") },
+    ],
+    locales: ["en", "ja"],
+    messages: {
+      en: {
+        message: "Hello",
+      },
+      ja: {
+        message: "こんにちわ",
+      },
     },
-    ja: {
-      message: "こんにちわ",
-    },
-  },
+  })
 })
