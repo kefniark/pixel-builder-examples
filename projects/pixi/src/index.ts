a9864b355e48b70835d9a5cb457d58dcf35699d8
Kefniark <kevin@destrem.net>
2022-06-08 21:25:21 +0900

    Update CLI version

--- projects/pixi/src/index.ts
+++ projects/pixi/src/index.ts
@@ -3,24 +3,20 @@ import App from "./app.vue"
 import IntroScene from "@src/scenes/intro.vue"
 import MenuScene from "@src/scenes/menu.vue"
 
-console.log('init')
-window.addEventListener('load', () => {
-  console.log('loaded')
-  // create Application
-  mountApp("#app", App, {
-    routes: [
-      { path: "/", component: IntroScene },
-      { path: "/menu", component: MenuScene },
-      { path: "/main", component: () => import("./scenes/main.vue") },
-    ],
-    locales: ["en", "ja"],
-    messages: {
-      en: {
-        message: "Hello",
-      },
-      ja: {
-        message: "こんにちわ",
-      },
+// create Application
+mountApp("#app", App, {
+  routes: [
+    { path: "/", component: IntroScene },
+    { path: "/menu", component: MenuScene },
+    { path: "/main", component: () => import("./scenes/main.vue") },
+  ],
+  locales: ["en", "ja"],
+  messages: {
+    en: {
+      message: "Hello",
     },
-  })
-});
+    ja: {
+      message: "こんにちわ",
+    },
+  },
+})
