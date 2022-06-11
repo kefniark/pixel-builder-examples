5680b12473f1307ff1632175a7eb6f5be814c555
Kefniark <kevin@destrem.net>
2022-06-11 12:40:09 +0900

    Update pixel/cli

--- build.sh
+++ build.sh
@@ -7,24 +7,20 @@ mkdir build
 # Pixi Example
 cd projects/pixi
 yarn
-yarn link "@pixel-builder/cli"
-yarn link "@pixel-builder/core"
-MSYS_NO_PATHCONV=1 yarn build
+yarn build
 cd ../..
 mv projects/pixi/build/web build/pixi
 
-cp -R build/pixi/* /c/Users/Kef/Downloads/pixel-builder-examples-gh-pages/local/pixi/
-
 # BabylonJS Example
-# cd projects/babylon
-# yarn
-# yarn build
-# cd ../..
-# mv projects/babylon/build/web build/babylon
+cd projects/babylon
+yarn
+yarn build
+cd ../..
+mv projects/babylon/build/web build/babylon
 
-# # Minimalist Example
-# cd projects/js13k
-# yarn
-# yarn build
-# cd ../..
-# mv projects/js13k/build/web build/js13k
+# Minimalist Example
+cd projects/js13k
+yarn
+yarn build
+cd ../..
+mv projects/js13k/build/web build/js13k
