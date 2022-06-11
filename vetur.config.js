62247b4e896b1da36d60244759cba0ed34238682
Kefniark <kevin@destrem.net>
2022-06-11 12:37:13 +0900

    Update pixel/cli

---
+++ vetur.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    settings: {
+        "vetur.useWorkspaceDependencies": true,
+        "vetur.experimental.templateInterpolationService": true
+    },
+    projects: [
+        './projects/babylon',
+        './projects/js13k',
+        './projects/pixi'
+    ]
+}
